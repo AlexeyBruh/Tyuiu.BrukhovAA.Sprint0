446afeb29b9289ff52a3e6d2e311afe0055cfc1b
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-09-13 21:20:47 +0500

    Task5

---
+++ Tyuiu.BrukhovAA.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BrukhovAA.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            //В реальных проектов используется Exception
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя");
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
+
---
+++ Tyuiu.BrukhovAA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrukhovAA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrukhovAA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("032dc80c-7f60-4a64-bd81-04f570d8d03e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
