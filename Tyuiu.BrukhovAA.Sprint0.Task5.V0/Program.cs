fbd015ae7160fb2ac0bdc67cd67563da41089806
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-09-14 20:35:47 +0500

    Task5

--- Tyuiu.BrukhovAA.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.BrukhovAA.Sprint0.Task5.V0/Program.cs
@@ -14,9 +14,9 @@ namespace Tyuiu.BrukhovAA.Sprint0.Task5.V0
         {
             Console.WriteLine("A + B = " + DataService.Addition(1, 5));
             Console.WriteLine("A + B = " + DataService.Substraction(15, 5));
-            Console.WriteLine("A + B = " + DataService.Multiplication(10, 5));
+            Console.WriteLine("A + B = " + DataService.Multiplication(10, 10));
 
-            Console.WriteLine("A + B = " + DataService.Division(5, 5));
+            Console.WriteLine("A + B = " + DataService.Division(5, 0));
             Console.ReadKey();
         }
     }
